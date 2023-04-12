90433e50df5571bb9ba93383fe84ecea242ef01b
Alexander Lakiza <285469@edu.itmo.ru>
2023-04-12 23:59:43 +0300

    Create streamlit dash structure

---
+++ 0_🏡_Домашняя_страница.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+if __name__ == "__main__":
+    st.set_page_config(
+        page_title="Реализация молодежной политики в РФ"
+    )
+
+    st.title("Это домашняя страница!!!")
+    st.write('haha')
---
+++ pages/2_📊_Дашборд.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+if __name__ == "__main__":
+    st.title("ВТОРАЯ!!!")
+    with st.sidebar:
+        st.write('А как же сайдбар?')
