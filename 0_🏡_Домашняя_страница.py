18dc9e511a0ed003a26b435ba582a266b5d4c915
Alexander Lakiza <285469@edu.itmo.ru>
2023-04-13 13:13:06 +0300

    Update 0_🏡_Домашняя_страница.py

--- 0_🏡_Домашняя_страница.py
+++ 0_🏡_Домашняя_страница.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
 if __name__ == "__main__":
+    # TODO: README
+    # TODO: Описание этой страницы
     st.set_page_config(
         page_title="Реализация молодежной политики в РФ"
     )
