9bbd33b47f0d8547febd931e954ca37d2176ff0b
Alexander Lakiza <285469@edu.itmo.ru>
2023-04-20 17:17:49 +0300

    dashboard page is ready

--- pages/2_📊_Дашборд.py
+++ pages/2_📊_Дашборд.py
@@ -72,11 +72,88 @@ def map_chart(data,
     return fig
 
 
+def bar_plot(data,
+             first_feature,
+             second_feature=None,
+             column_of_second_feature=None,
+             legend_text_size=12,
+             xaxis_title_slice=None):
+    if second_feature == 'Все' or second_feature is None:
+        mini_grouped_df = data.groupby(['Регион', 'Показатель']).sum(numeric_only=True).reset_index()
+
+        top_regions = mini_grouped_df[mini_grouped_df['Показатель'] ==
+                                      first_feature].sort_values(by=['Значение'],
+                                                                 ascending=False)[:10]['Регион'].tolist()
+
+        if column_of_second_feature:
+
+            temp_df = data[(data['Показатель'] == first_feature) &
+                           (data['Регион'].isin(top_regions))].copy()
+
+            fig = go.Figure()
+
+            for second_feature in temp_df[column_of_second_feature].unique():
+                fig.add_trace(go.Bar(
+                    x=temp_df[temp_df[column_of_second_feature] == second_feature]['Значение'],
+                    y=temp_df[temp_df[column_of_second_feature] == second_feature]['Регион'],
+                    orientation='h',
+                    name=break_long_string(second_feature),
+                    hovertemplate=f"{main_option}<br><b>%{{y}}</b><br>Значение = %{{x}}<extra></extra>"
+                ))
+
+            fig.update_layout(barmode='stack',
+                              margin={"r": 0, "t": 10, "l": 0, "b": 0},
+                              legend=dict(font=dict(size=legend_text_size)))
+            fig.update_xaxes(title=first_feature)
+            return fig
+
+        else:
+            temp_df = mini_grouped_df[(mini_grouped_df['Показатель'] == first_feature) &
+                                      (mini_grouped_df['Регион'].isin(top_regions))].copy()
+
+            fig = go.Figure()
+
+            fig.add_trace(go.Bar(
+                x=temp_df['Значение'],
+                y=temp_df['Регион'],
+                orientation='h',
+                name=first_feature,
+                hovertemplate=f"{main_option}<br><b>%{{y}}</b><br>Значение = %{{x}}<extra></extra>"
+            ))
+
+            fig.update_layout(margin={"r": 0, "t": 10, "l": 0, "b": 0},
+                              legend=dict(font=dict(size=legend_text_size)))
+            fig.update_xaxes(title=first_feature)
+            return fig
+
+    else:
+        temp_df = data[(data['Показатель'] == first_feature) &
+                       (data[column_of_second_feature] ==
+                        second_feature)].sort_values(by=['Значение'],
+                                                     ascending=False)[:10].copy()
+
+        fig = go.Figure()
+
+        fig.add_trace(go.Bar(
+            x=temp_df['Значение'],
+            y=temp_df['Регион'],
+            orientation='h',
+            name=second_feature,
+            hovertemplate=f"{main_option}<br><b>%{{y}}</b><br>Значение = %{{x}}<extra></extra>"
+        ))
+
+        fig.update_layout(margin={"r": 0, "t": 10, "l": 0, "b": 0},
+                          legend=dict(font=dict(size=legend_text_size)))
+        fig.update_xaxes(title=f"{xaxis_title_slice} = {second_feature}")
+        return fig
+
+
 def section_for_map(data,
                     second_slice_of_title_all=None,
                     second_slice_of_title_specific=None,
                     checkbox_label=None,
-                    column_of_second_feature=None):
+                    column_of_second_feature=None,
+                    bar_plot_xaxis_slice=None):
     if column_of_second_feature:
         second_feature_options = data[column_of_2nd_feature].unique().tolist()
         second_option = st.selectbox(
@@ -90,9 +167,6 @@ def section_for_map(data,
 
             grouped_df = df.groupby(['Регион', 'Показатель']).sum(numeric_only=True).reset_index()
             table_to_show = grouped_df[grouped_df['Показатель'] == main_option]
-
-            top_regions = table_to_show.sort_values(by=['Значение'], ascending=False)[:10]['Регион'].tolist()
-
 
         else:
             second_slice_of_title = f" {second_slice_of_title_specific} \"_{second_option}_\""
@@ -115,9 +189,28 @@ def section_for_map(data,
                                   colors=color_palette),
                         use_container_width=True)
 
-        st.markdown("##### Топ 10 регионов по выбранному показателю")
-
-        st.plotly_chart()
+        st.markdown(f"##### Топ 10 регионов по выбранному показателю \"{main_option}\"")
+        if second_option == 'Все':
+            tab3, tab5 = st.tabs(['Диаграмма без детализации', 'С детализацией'])
+            with tab3:
+                st.plotly_chart(bar_plot(data=df,
+                                         first_feature=main_option,
+                                         second_feature=second_option),
+                                use_container_width=True)
+            with tab5:
+                st.plotly_chart(bar_plot(data=df,
+                                         first_feature=main_option,
+                                         second_feature=second_option,
+                                         column_of_second_feature=column_of_2nd_feature,
+                                         legend_text_size=9),
+                                use_container_width=True)
+        else:
+            st.plotly_chart(bar_plot(data=df,
+                                     first_feature=main_option,
+                                     second_feature=second_option,
+                                     column_of_second_feature=column_of_2nd_feature,
+                                     xaxis_title_slice=bar_plot_xaxis_slice),
+                            use_container_width=True)
 
         show_table = st.checkbox(label='Таблица с данными',
                                  help='Показывать таблицу с данными по выбранному показателю?',
@@ -132,6 +225,11 @@ def section_for_map(data,
         st.markdown(map_title_slice)
         st.plotly_chart(map_chart(data=df, geodata=geojson, first_feature=main_option, map_style=mapbox_style,
                                   colors=color_palette),
+                        use_container_width=True)
+
+        st.markdown("##### Топ 10 регионов по выбранному показателю")
+        st.plotly_chart(bar_plot(data=df,
+                                 first_feature=main_option),
                         use_container_width=True)
 
         show_table = st.checkbox(label='Таблица с данными',
@@ -150,7 +248,6 @@ def break_long_string(string):
 
         for i in range(15):
             if string[median + i] == ' ':
-                print(median + i)
                 return string[:median + i] + '<br>' + string[median + i + 1:]
     return string
 
@@ -193,7 +290,7 @@ def section_for_pie_chart(data, header, column_name):
     if show_table:
         st.markdown(f'##### Таблица значений показателя \"{main_feature_for_pie}\"')
         st.info("Вы можете отсортировать записи в таблице, нажав на поле. Также вы можете увеличить размер "
-                    "таблицы, потянув за правый нижний угол")
+                "таблицы, потянув за правый нижний угол")
         st.dataframe(temp_df.drop(['Округ', 'Показатель'], axis=1))
 
 
@@ -262,7 +359,8 @@ if __name__ == "__main__":
                         second_slice_of_title_all="по всем направлениям",
                         second_slice_of_title_specific="по направлению",
                         checkbox_label="Выберите направление реализации молодежной политики",
-                        column_of_second_feature=column_of_2nd_feature)
+                        column_of_second_feature=column_of_2nd_feature,
+                        bar_plot_xaxis_slice='Направление')
 
         section_for_pie_chart(header=f"#### Распределение значений среди направлений "
                                      f"для показателя \"{column_of_2nd_feature}\"",
@@ -275,7 +373,8 @@ if __name__ == "__main__":
                         second_slice_of_title_all="для всех видов структур",
                         second_slice_of_title_specific="для вида структур",
                         checkbox_label="Выберите вид структуры по работе с молодежью",
-                        column_of_second_feature=column_of_2nd_feature)
+                        column_of_second_feature=column_of_2nd_feature,
+                        bar_plot_xaxis_slice='Структура')
 
         section_for_pie_chart(header=f"#### Распределение значений среди видов структур "
                                      f"для показателя \"{column_of_2nd_feature}\"",
@@ -286,25 +385,19 @@ if __name__ == "__main__":
         section_for_map(data=df)
 
     elif section == 'Общественные объединения':  # P4
-        tab1, tab2 = st.tabs(["Объединения", "Объединения на базе образовательных организаций"])
-        with tab1:
-            df = pd.read_parquet('data/p4.parquet')
-            column_of_2nd_feature = columns_of_2nd_feature[section]
+        column_of_2nd_feature = columns_of_2nd_feature[section]
 
-            section_for_map(data=df,
-                            second_slice_of_title_all="для всех видов общ. объединений",
-                            second_slice_of_title_specific="по виду объединений",
-                            checkbox_label="Выберите вид молодежных объединений",
-                            column_of_second_feature=column_of_2nd_feature)
+        section_for_map(data=df,
+                        second_slice_of_title_all="для всех видов общ. объединений",
+                        second_slice_of_title_specific="по виду объединений",
+                        checkbox_label="Выберите вид молодежных объединений",
+                        column_of_second_feature=column_of_2nd_feature,
+                        bar_plot_xaxis_slice='Объединение')
 
-            section_for_pie_chart(header=f"#### Распределение значений среди видов объединений "
-                                         f"для показателя \"{column_of_2nd_feature}\"",
-                                  data=df,
-                                  column_name=column_of_2nd_feature)
-
-        with tab2:
-            df = pd.read_parquet('data/p4-2.parquet')
-            section_for_map(data=df)
+        section_for_pie_chart(header=f"#### Распределение значений среди видов объединений "
+                                     f"для показателя \"{column_of_2nd_feature}\"",
+                              data=df,
+                              column_name=column_of_2nd_feature)
 
     elif section == 'Общественные объединения на базе образовательных учреждений':  # P4-2
         section_for_map(data=df)
@@ -316,7 +409,8 @@ if __name__ == "__main__":
                         second_slice_of_title_all="для всех видов форумов",
                         second_slice_of_title_specific="для вида форумов",
                         checkbox_label="Выберите вид молодежных форумов",
-                        column_of_second_feature=column_of_2nd_feature)
+                        column_of_second_feature=column_of_2nd_feature,
+                        bar_plot_xaxis_slice='Форум')
 
         section_for_pie_chart(header=f"#### Распределение значений среди видов форумов "
                                      f"для показателя \"{column_of_2nd_feature}\"",
